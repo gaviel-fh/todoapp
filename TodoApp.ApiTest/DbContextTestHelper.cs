2de4cd574ca82ed74e84bad36dead988fb17e7f2
David Raese <david.raese@protonmail.com>
2023-03-19 11:13:36 +0100

    created first tests for the todoItems controller

---
+++ TodoApp.ApiTest/DbContextTestHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Api.Data;
+using TodoApp.Api.Entities;
+
+namespace TodoApp.ApiTest
+{
+    public static class DbContextTestHelper
+    {
+        public static DataContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+
+            var context = new DataContext(options);
+
+            // Add test data
+            context.TodoItems.Add(new TodoItem { Id = 1, Title = "Test item 1", IsCompleted = false });
+            context.TodoItems.Add(new TodoItem { Id = 2, Title = "Test item 2", IsCompleted = true });
+            context.SaveChanges();
+
+            return context;
+        }
+    }
+}
---
+++ TodoApp.ApiTest/TodoItemsControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Api.Controllers;
+using TodoApp.Api.Entities;
+
+namespace TodoApp.ApiTest
+{
+    public class TodoItemsControllerTests
+    {
+        [Fact]
+        public async Task GetTodoItems_ReturnsAllItems()
+        {
+            // Arrange
+            var context = DbContextTestHelper.GetInMemoryDbContext();
+            var controller = new TodoItemsController(context);
+
+            // Act
+            var result = await controller.GetTodoItems();
+
+            // Assert
+            Assert.Equal(2, result.Value.Count());
+        }
+
+        [Fact]
+        public async Task GetTodoItem_ReturnsItem_WhenIdExists()
+        {
+            // Arrange
+            var context = DbContextTestHelper.GetInMemoryDbContext();
+            var controller = new TodoItemsController(context);
+            int existingItemId = 1;
+
+            // Act
+            var result = await controller.GetTodoItem(existingItemId);
+
+            // Assert
+            Assert.IsType<ActionResult<TodoItem>>(result);
+            Assert.Equal(existingItemId, result.Value.Id);
+        }
+
+        [Fact]
+        public async Task GetTodoItem_ReturnsNotFound_WhenIdDoesNotExist()
+        {
+            // Arrange
+            var context = DbContextTestHelper.GetInMemoryDbContext();
+            var controller = new TodoItemsController(context);
+            int nonExistentItemId = 999;
+
+            // Act
+            var result = await controller.GetTodoItem(nonExistentItemId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+    }
+}
